0d005879433d3a3433e4aca0450c3a8e85277a3e
Patrick Leary <patrick.r.leary@gmail.com>
2026-09-25 11:58:51 -0400

    chore: some small changes for rails 7.x

--- lib/generators/preferences/preferences_generator.rb
+++ lib/generators/preferences/preferences_generator.rb
@@ -4,7 +4,7 @@ class PreferencesGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
 
   def self.next_migration_number(dirname)
-    if ActiveRecord::Base.timestamped_migrations
+    if ActiveRecord.timestamped_migrations
       Time.now.utc.strftime("%Y%m%d%H%M%S")
     else
       "%.3d" % (current_migration_number(dirname) + 1)
